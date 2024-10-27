04197eb0bdc4288bfbc0f0f7e5641dff81015b41
liuyaowen <liuyaowen_smile@126.com>
2024-10-27 13:47:11 +0800

    add cors

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["www.liuyaowen.cn", "liuyaowen.cn"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
